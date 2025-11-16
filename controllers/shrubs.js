dc1ff26db5a22cb27b118860bdbfc15c2110b1ce
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-11-16 18:49:22 -0500

    feat: implement CRUD operations for shrubs and trees controllers

--- controllers/shrubs.js
+++ controllers/shrubs.js
@@ -4,9 +4,9 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
   //#swagger.tags-['shrubs']
   const result = await mongodb.getDatabase().db().collection('shrubs').find();
-  result.toArray().then((ushrubs) => {
+  result.toArray().then((shrubs) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(ushrubs);
+    res.status(200).json(shrubs);
   });
 };
 
@@ -14,13 +14,74 @@ const getSingle = async (req, res) => {
   //#swagger.tags-['shrubs']
   const shrubsId = new ObjectId(req.params.id);
   const result = await mongodb.getDatabase().db().collection('shrubs').find({ _id: shrubsId });
-  result.toArray().then((ushrubs) => {
+  result.toArray().then((shrubs) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(ushrubs[0]);
+    res.status(200).json(shrubs[0]);
   });
+};
+
+const createShrubs = async (req, res) => {
+  //#swagger.tags-['Shrubs']
+  const shrubs = {
+    plantName: req.body.plantName,
+    family: req.body.family,
+    genus: req.body.genus,
+    species: req.body.species,
+    cultivar: req.body.cultivar,
+    hardinessZone: req.body.hardinessZone,
+    flower: req.body.flower,
+    leaf: req.body.leaf,
+    growthHabit: req.body.growthHabit
+  };
+  const response = await mongodb.getDatabase().db().collection('shrubs').insertOne(shrubs);
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the shrubs.');
+  }
+};
+
+const updateShrubs= async (req, res) => {
+  //#swagger.tags-['shrubs']
+  const shrubsId = new ObjectId(req.params.id);
+  const shrubs = {
+    plantName: req.body.plantName,
+    family: req.body.family,
+    genus: req.body.genus,
+    species: req.body.species,
+    cultivar: req.body.cultivar,
+    hardinessZone: req.body.hardinessZone,
+    flower: req.body.flower,
+    leaf: req.body.leaf,
+    growthHabit: req.body.growthHabit
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection('shrubs')
+    .replaceOne({ _id: shrubsId }, shrubs);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the shrubs.');
+  }
+};
+
+const deleteShrubs = async (req, res) => {
+  //#swagger.tags-['Shrubs']
+  const shrubsId = new ObjectId(req.params.id);
+  const response = await mongodb.getDatabase().db().collection('shrubs').deleteOne({ _id: shrubsId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the shrubs.');
+  }
 };
 
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createShrubs,
+  updateShrubs,
+  deleteShrubs
 };
--- controllers/trees.js
+++ controllers/trees.js
@@ -20,7 +20,68 @@ const getSingle = async (req, res) => {
   });
 };
 
+const createTrees = async (req, res) => {
+  //#swagger.tags-['Trees']
+  const trees = {
+    plantName: req.body.plantName,
+    family: req.body.family,
+    genus: req.body.genus,
+    species: req.body.species,
+    cultivar: req.body.cultivar,
+    hardinessZone: req.body.hardinessZone,
+    flower: req.body.flower,
+    leaf: req.body.leaf,
+    growthHabit: req.body.growthHabit
+  };
+  const response = await mongodb.getDatabase().db().collection('trees').insertOne(trees);
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the trees.');
+  }
+};
+
+const updateTrees= async (req, res) => {
+  //#swagger.tags-['trees']
+  const treesId = new ObjectId(req.params.id);
+  const trees = {
+    plantName: req.body.plantName,
+    family: req.body.family,
+    genus: req.body.genus,
+    species: req.body.species,
+    cultivar: req.body.cultivar,
+    hardinessZone: req.body.hardinessZone,
+    flower: req.body.flower,
+    leaf: req.body.leaf,
+    growthHabit: req.body.growthHabit
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection('trees')
+    .replaceOne({ _id: treesId }, trees);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the trees.');
+  }
+};
+
+const deleteTrees = async (req, res) => {
+  //#swagger.tags-['Trees']
+  const treesId = new ObjectId(req.params.id);
+  const response = await mongodb.getDatabase().db().collection('trees').deleteOne({ _id: treesId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the trees.');
+  }
+};
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createTrees,
+  updateTrees,
+  deleteTrees
 };
