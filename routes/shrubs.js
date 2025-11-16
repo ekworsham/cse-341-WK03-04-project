018c9b20868b2cde505d2bb3779055ad998c55f8
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-11-16 18:58:50 -0500

    feat: add CRUD operations for shrubs and trees routes

--- routes/shrubs.js
+++ routes/shrubs.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const shrubsController = require('../controllers/shrubs');
 
 router.get('/', shrubsController.getAll);
+
 router.get('/:id', shrubsController.getSingle);
+
+router.post('/', shubssController.createShubs);
+
+router.put('/:id', shubssController.updateShubs);
+
+router.delete('/:id', shubssController.deleteShubs);
+
 
 module.exports = router;
--- routes/trees.js
+++ routes/trees.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const treesController = require('../controllers/trees');
 
 router.get('/', treesController.getAll);
+
 router.get('/:id', treesController.getSingle);
+
+router.post('/', treessController.createTrees);
+
+router.put('/:id', treessController.updateTrees);
+
+router.delete('/:id', treessController.deleteTrees);
 
 module.exports = router;
