89bc44a2fd22524b39c5ece5d1ec0c87551e45e1
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-11-24 19:49:15 -0500

    fix: update authentication error message for clarity

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).json({ error: 'WHATS UP!!! You have NOT been authenticated' });
+    return res.status(401).json({ error: 'WHATS UP!!! Authenticated you are NOT?' });
   }
   next();
 };
